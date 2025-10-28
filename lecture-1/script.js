ccfb2dd98194d576d65aba828413b41cdd2eb6e2
Fredrik Welander <welandfr@arcada.fi>
2025-10-28 15:52:29 +0200

    Lecture 1

---
+++ lecture-1/script.js
@@ -0,0 +1,13 @@
+console.log("The .js file works!");
+
+function myFunction() {
+    console.log("The function works!");
+
+    let a = 5;
+
+    console.log(a);
+}
+
+//console.log(a);
+
+myFunction();
