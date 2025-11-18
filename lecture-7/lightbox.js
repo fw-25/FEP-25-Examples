b12d2cfe9c5b66dee0e97f45d1cecec015ac6fe6
Fredrik Welander <welandfr@arcada.fi>
2025-11-18 15:42:59 +0200

    fixes

--- lecture-7/lightbox.js
+++ lecture-7/lightbox.js
@@ -2,7 +2,7 @@ const IMAGES = [16, 28, 69, 51];
 for (const imgId of IMAGES) {
     document.getElementById("thumbnails").innerHTML += `
         <img data-id="${imgId}" 
-        src="./lightbox-img/thumb-img-0${imgId}.png">
+            src="./lightbox-img/thumb-img-0${imgId}.png">
     `;
 }
 
@@ -10,7 +10,7 @@ document.querySelectorAll("#thumbnails img").forEach(thumb => {
     console.log(thumb);
     thumb.addEventListener('click', () => {
         const imgId = thumb.dataset.id;
-        console.log(imgId)
+        //console.log(imgId)
         document.getElementById("lightbox").innerHTML = `
             <img src="./lightbox-img/img-0${imgId}.png">
         `;
