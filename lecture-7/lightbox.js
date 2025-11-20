f466209666732ec72a89243acf23d4f842153ad7
Fredrik Welander <welandfr@arcada.fi>
2025-11-20 13:35:48 +0200

    add lightbox v2

--- lecture-7/lightbox.js
+++ lecture-7/lightbox.js
@@ -1,4 +1,4 @@
-const IMAGES = [16, 28, 69, 51];
+const IMAGES = [16, 28, 69, 51, 49, 61];
 for (const imgId of IMAGES) {
     document.getElementById("thumbnails").innerHTML += `
         <img data-id="${imgId}" 
@@ -6,13 +6,31 @@ for (const imgId of IMAGES) {
     `;
 }
 
+function openLightbox() {
+    document.getElementById("lightbox").classList.remove("hidden");
+}
+function closeLightbox() {
+    document.getElementById("lightbox").classList.add("hidden");
+}
+
 document.querySelectorAll("#thumbnails img").forEach(thumb => {
     console.log(thumb);
     thumb.addEventListener('click', () => {
         const imgId = thumb.dataset.id;
-        //console.log(imgId)
+        
         document.getElementById("lightbox").innerHTML = `
             <img src="./lightbox-img/img-0${imgId}.png">
         `;
+        openLightbox();
+        
     });
+});
+
+document.getElementById("lightbox").addEventListener('click', closeLightbox);
+window.addEventListener('keydown', (evt) => {
+    if (!document.getElementById("lightbox").classList.contains("hidden")
+        && evt.key === "Escape") {
+        closeLightbox();
+    }
+    
 });
